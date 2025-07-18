f93521fa242be17d3b3583cf4c3969a3f0a209ea
anadolsk <anadolsk@macalester.edu>
2025-07-18 10:02:24 -0500

    completed services section

---
+++ src/sections/services/data.js
@@ -0,0 +1,13 @@
+import {SiAdobexd} from 'react-icons/si'
+import {RiReactjsLine} from 'react-icons/ri'
+import { FaServer } from 'react-icons/fa'
+import { AiFillAppstore } from 'react-icons/ai'
+
+const data = [
+    {id: 1, icon: <SiAdobexd/>, title: "UI/UX Design", desc: "I can make good designs."},
+    {id: 2, icon: <RiReactjsLine/>, title: "Frontend Development", desc: "I can make products accessible on all devices."},
+    {id: 3, icon: <FaServer/>, title: "Backend Development", desc: "I can make a secure product."},
+    {id: 4, icon: <AiFillAppstore/>, title: "App Development", desc: "I can makke IOS and Android apps."},
+]
+
+export default data
