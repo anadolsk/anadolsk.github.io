0e87811ab5118aa3f60bdfab0b27c121d14dd142
anadolsk <anadolsk@macalester.edu>
2025-09-20 23:26:02 -0500

    removed floating nav bar

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,6 @@
 import About from "./sections/about/About"
 import Contact from "./sections/contact/Contact";
 import FAQs from "./sections/faqs/FAQs";
-import FloatingNav from "./sections/floating-nav/FloatingNav";
 import Footer from "./sections/footer/Footer";
 import Header from "./sections/header/Header"
 import Navbar from "./sections/navbar/Navbar"
@@ -10,44 +9,13 @@ import Services from './sections/services/Services';
 import Testimonials from "./sections/testimonials/Testimonials";
 import Theme from './theme/Theme';
 import { useThemeContext } from './context/theme-context';
-import {useRef, useState, useEffect} from 'react'
+import {useRef} from 'react'
 
 const App = () => {
   const {themeState} = useThemeContext();
 
   const mainRef = useRef();
-  const [showFloatingNav, setShowFloatingNav] = useState(true);
-  const [siteYPostion, setSiteYPosition] = useState(0)
-
-  const showFloatingNavHandler = () => {
-    setShowFloatingNav(true);
-  }
-
-  const hideFloatingNavHandler = () => {
-    setShowFloatingNav(false);
-  }
-
-  // check if floating nav should be shown or hidden
-  const floatingNavToggleHandler = () => {
-    // check if we scrolled up or down at least 20px
-    if(siteYPostion < (mainRef?.current?.getBoundingClientRect().y - 20) || siteYPostion > (mainRef?.current?.getBoundingClientRect().y + 20)) {
-      showFloatingNavHandler();
-    } else {
-      hideFloatingNavHandler();
-    }
-
-    setSiteYPosition(mainRef?.current?.getBoundingClientRect().y);
-  }
-
-  useEffect(() => {
-    const checkYPosition = setInterval(floatingNavToggleHandler, 2000);
-
-    // cleanup function
-    return () => clearInterval(checkYPosition);
-  }, [siteYPostion])
-
   
-
   return (
     <main className={`${themeState.primary} ${themeState.background}`} ref={mainRef}>
         <Navbar/>
@@ -60,7 +28,7 @@ const App = () => {
         <Contact/>
         <Footer/>
         <Theme/>
-        {showFloatingNav && <FloatingNav/>}
+        
     </main>
   )
 }
